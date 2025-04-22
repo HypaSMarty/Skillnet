b0c820f52cb39b8332b1f64697a1fbbd725e5a37
Tinashe Shumba <tshumba@nrfa.org.zm>
2025-04-22 09:16:04 +0200

    Initial commit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,21 +1,18 @@
-allprojects {
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
+rootProject.name = "skillnet_app"
+include(":app")
